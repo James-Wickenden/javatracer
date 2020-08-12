ad74d1eac4ac5994d1fdef20ae6fa08c6570587e
James Wickenden <jameswickenden14@gmail.com>
2020-08-12 17:43:33 +0100

    refactored and continued experimenting

---
+++ src/javatracer/Tracer.java
@@ -0,0 +1,37 @@
+package javatracer;
+
+import javax.swing.SwingUtilities;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.BorderFactory;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics; 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseMotionAdapter;
+
+public class Tracer {
+    
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI(); 
+            }
+        });
+    }
+
+    private static void createAndShowGUI() {
+        System.out.println("Created GUI on EDT? "+
+        SwingUtilities.isEventDispatchThread());
+        JFrame f = new JFrame("Swing Paint Demo");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+        f.add(new RayPanel());
+        f.setSize(250,250);
+        f.setVisible(true);
+    } 
+
+}
