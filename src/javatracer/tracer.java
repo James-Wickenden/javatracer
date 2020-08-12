10f577427262937bb98131f45419da5dfbc5ba6f
James Wickenden <jameswickenden14@gmail.com>
2020-08-12 16:54:34 +0100

    canvas with painting working

--- src/javatracer/tracer.java
+++ src/javatracer/tracer.java
@@ -1,28 +1,10 @@
 package javatracer;
-import java.awt.Canvas;
-import java.awt.Graphics;
-import javax.swing.JFrame;
 
 public class tracer {
-	static Graphics graphics;
-	static Canvas canvas;
-	
 	public static void main(String[] args) {
-		System.out.println("Test");
+		System.out.println("test II");
 		
-		JFrame frame = new JFrame("My Drawing");
-        canvas = new Canvas();
-        graphics = canvas.getGraphics();
-        canvas.setSize(400, 400);
-        frame.add(canvas);
-        frame.pack();
-        frame.setVisible(true);
-        
-        paint();
-        canvas.update(graphics);
+		Drawing drawing = new Drawing();
+		
 	}
-	
-	public static void paint() {
-        graphics.fillOval(100, 100, 200, 200);
-    }
 }
