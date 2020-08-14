775399a484190f97186e3d8a382503271b3755f8
James Wickenden <jameswickenden14@gmail.com>
2020-08-14 11:18:19 +0100

    added a 3d triangle class

---
+++ src/javatracer/ModelTriangle.java
@@ -0,0 +1,40 @@
+package javatracer;
+
+import java.awt.Color;
+import com.sun.javafx.geom.Vec3f;
+
+public class ModelTriangle {
+	private Vec3f[] vertices;
+	private Color colour;
+	
+	public ModelTriangle() {
+		for (Vec3f vertex : vertices) {
+			vertex = new Vec3f(0.0f, 0.0f, 0.0f);
+		}
+		colour = Color.MAGENTA;
+	}
+	public ModelTriangle(Vec3f vertices[]) {
+		this.vertices = vertices;
+		colour = Color.MAGENTA;
+	}
+	public ModelTriangle(Vec3f vertices[], Color colour) {
+		this.vertices = vertices;
+		this.colour = colour;
+	}
+	
+	public Vec3f[] GetVertices() {
+		return vertices;
+	}
+	
+	public void SetVertices(Vec3f[] vertices) {
+		this.vertices = vertices;
+	}
+	
+	public Color GetColour() {
+		return colour;
+	}
+	
+	public void SetColour(Color colour) {
+		this.colour = colour;
+	}
+}
